a3c3972e9b3ace7759a1952575c31b2b49bb763a
aasif112 <engr.aasifali@gmail.com>
2024-07-28 20:21:08 +0500

    last push

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ function App() {
   return (
     <div >
       <h1>Hello World</h1>
-      <h3>Here we have developed first React-App page.</h3>
-      <h3>The Page is deployed on surge.sh</h3>
-      <h3>A GitHub work-flow is setup that starts on: push</h3>
-      <h3>Push: 28-07-2024</h3>
+      <ul>
+        <li>
+          <h3>Here we have developed first React-App page.</h3>
+        </li>
+        <li>
+        <h3>The Page is deployed on surge.sh</h3>
+        </li>
+        <li>
+        <h3>A GitHub work-flow is setup that starts on: push</h3>
+        </li>
+        <li>
+        <h3>Push: 28-07-2024</h3>
+        </li>
+      </ul>
     </div>
   );
 }
